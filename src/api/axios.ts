98d81cfbc07c2eb33dfa04d91d98d6fe1d2e5ebb
Zainab Omolola Fadeyi <70825458+Zainabfadeyi@users.noreply.github.com>
2024-09-26 17:21:27 +0100

    push

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
     // baseURL:"http://localhost:8080"
-    baseURL:"https://final-ugy3.onrender.com"
+    baseURL:"https://13.61.23.60:8080"
 })
 
 instance.interceptors.request.use(
