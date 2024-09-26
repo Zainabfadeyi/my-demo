d3996b96ce532a25ef90be8161b0e9b075ee6b45
Zainab Omolola Fadeyi <70825458+Zainabfadeyi@users.noreply.github.com>
2024-09-26 17:23:06 +0100

    Update axios.ts

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
     // baseURL:"http://localhost:8080"
-    baseURL:"https://13.61.23.60:8080"
+    baseURL:"http://13.61.23.60:8080"
 })
 
 instance.interceptors.request.use(
